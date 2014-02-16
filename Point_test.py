600ef79c1e9c5107afb96bfde87b09eed1486326
Ladinu Chandrasinghe <ladinu@gmail.com>
2014-02-16 08:20:05 -0800

    Added comparison tests

--- Point_test.py
+++ Point_test.py
@@ -19,6 +19,20 @@ class PointTest(unittest.TestCase):
       self.assertEqual(4, p.x)
       self.assertEqual(3, p.y)
 
+   def testEquality(self):
+      self.assertEqual(Point(1, 3), Point(1, 3))
+      self.assertEqual(Point(3, 3), Point(2, 2))
+      self.assertNotEqual(Point(1, 2), Point(1, 3))
+
+   def testLessThan(self):
+      self.assertTrue(Point(0, 3) < Point(0, 4))
+      self.assertTrue(Point(0, 1) < Point(0, 4))
+      self.assertFalse(Point(0, 4) < Point(0, 2))
+
+   def testGreaterThan(self):
+      self.assertTrue(Point(0, 2) > Point(0, 1))
+      self.assertFalse(Point(0, 1) > Point(0, 2))
+
 def f(p):
    p.x = 1
    p.y = 2
