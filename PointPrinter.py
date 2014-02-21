793a91cca8584474ae38e80b9ea92ea36dfdb727
Ladinu Chandrasinghe <ladinu@gmail.com>
2014-02-20 16:39:28 -0800

    Now pointprinter can print points to file. Still need to add more unit tests

--- PointPrinter.py
+++ PointPrinter.py
@@ -5,31 +5,43 @@ from Point import Point as P
 class PointPrinterException(Exception):
    pass
 
+class PointPrinterPoint(P):
+   def __init__(self, point=P(), isMarked = False):
+      self.marked = isMarked
+      super(P, self).__new__(P, point.x, point.y)
+
+
 class PointPrinter():
    def __init__(self):
       self._points = []
 
    def printPoint(self, point):
-      self._points.append(point)
+      self._points.append(self._ppp(point))
 
    def printPoints(self, pointList):
-      self._points = self._points + pointList
+      points = []
+      for p in pointList:
+         points.append(self._ppp(p))
+      self._points = self._points + points
 
    def printToFile(self, outFile, outputType='image'):
       if outputType == 'image':
-         self._printImage(outFile)
+         self._writeImgToFile(outFile)
       else:
          raise PointPrinterException("invalid output type")
 
-   def _printImage(self, outFile):
+   def _composeImage(self):
       size = self._getImageSizeThatWillContainAllPoints()
       img = Image.new('RGB', size, 'white')
       draw = ImageDraw.Draw(img)
       for p in self._points:
-         bounds = self._flatten(getSquareEnclosingPoint(p))
-         print bounds
+         bounds = self._flatten(getSquareEnclosingPoint(p, 1))
          draw.rectangle(bounds, fill='black')
-      img.show()
+      return img
+
+   def _writeImgToFile(self, outFile):
+      img = self._composeImage()
+      img.save(outFile + '.bmp')
 
    def _flatten(self, bounds):
       return (bounds[0][0], bounds[0][1], bounds[1][0], bounds[1][1])
@@ -38,3 +50,5 @@ class PointPrinter():
       maxPoint = max(self._points)
       return P(maxPoint.x + padding, maxPoint.y + padding)
 
+   def _ppp(self, point):
+      return PointPrinterPoint(point)
