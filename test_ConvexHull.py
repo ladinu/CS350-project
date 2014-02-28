9feabc924d92343090b49877d91c765c85664356
Ladinu Chandrasinghe <ladinu@gmail.com>
2014-02-28 10:57:26 -0800

    Added tests

--- test_ConvexHull.py
+++ test_ConvexHull.py
@@ -2,7 +2,7 @@ import unittest
 import ConvexHull
 from PointUtils import *
 from Geometry import Point as P, Line
-import pyhull
+from pyhull.convex_hull import ConvexHull as pyConvexHull
 
 #Test convex hull results against pyhull's ConvexHull
 class ConvexHullTest(unittest.TestCase):
@@ -15,16 +15,15 @@ class ConvexHullTest(unittest.TestCase):
       self.assertEqual(1, len(vertices))
       self.assertTrue(vertices[0] == line)
 
-   @unittest.skip("broken")
    def test_Triangle(self):
       pointA = P(0, 0)
       pointB = P(0, 5)
       pointC = P(5, 0)
       triangle = [pointA, pointB, pointC]
-      bruteConvex = ConvexHull.computeHull(triangle)
-      pyhullConvex = convex_hull.Convexhull(triangle)
+      bruteConvex = [i.toList() for i in ConvexHull.computeHull(triangle)]
+      pyhullConvex = pyConvexHull(triangle)
       #compare using sets(list) because order might be different
-      self.assertTrue(set(bruteConvex.getVertices()) == set(pyhullConvex.vertices()))
+      self.assertTrue(sameList(bruteConvex, pyhullConvex.vertices))
 
    @unittest.skip("broken")
    def test_randomListPoints(self):
@@ -33,6 +32,17 @@ class ConvexHullTest(unittest.TestCase):
       pyhullConvex = convex_hull.Convexhull(randomPoints)
       #compare using sets(list) because order might be different
       self.assertTrue(set(bruteConvex.getVertices()) == set(pyhullConvex.vertices()))
+
+
+def sameList(listA, listB):
+   for a in listA:
+      if a not in listB:
+         return False
+   for b in listB:
+      if b not in listA:
+         return False
+   return True
+
 
 if __name__ == '__main__':
    unittest.main()
