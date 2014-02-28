a6c8476dfd0b0bd2f76ee5674b868d30ee9905f1
Ladinu Chandrasinghe <ladinu@gmail.com>
2014-02-27 19:09:59 -0800

    Reverted to old version remobing the debug code

--- hullDrive.py
+++ hullDrive.py
@@ -4,8 +4,6 @@ from GeometryUtils import *
 from PointUtils import *
 import matplotlib.pyplot as plt
 
-P = Point
-
 def flatten(points):
    xList = []
    yList = []
@@ -14,17 +12,13 @@ def flatten(points):
       yList.append(p.y)
    return (xList, yList)
 
-points = [P(25, 25), P(7, 41), P(13, 14), P(0, 49), P(45, 12), P(49, 35), P(24, 42), P(10, 1), P(22, 3), P(32, 12)]
-
-#points = getNRandomPoints(10, 0, 50)
-print "Random points: ", points
+points = getNRandomPoints(10, 0, 50)
 p1, p2 = flatten(points)
 
 plt.plot(p1, p2, 'o')
 plt.axis([-10, 60, -10, 60])
 
 simplices = ConvexHull.computeHull(points)
-simplices = [ i for i in simplices if not i.isPoint() ]
 print simplices
 for s in simplices:
    x1 = s.startPoint.x
